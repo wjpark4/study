558d7afd72eb274630ee0e692bd4392c9d433ed5
박우재 <@DESKTOP-5O4ET8N>
2020-06-24 20:35:22 +0900

    배열

---
+++ src/exercise/ArrayEx11.java
@@ -0,0 +1,20 @@
+package exercise;
+
+public class ArrayEx11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] number = {1, 2, 3, 4, 5};
+		int[] newNumber = new int[10];
+		
+		for(int i=0; i<number.length; i++){
+			newNumber[i] = number[i];
+		}
+		
+		for(int i=0; i<newNumber.length; i++){
+			System.out.println(newNumber[i]);
+		}
+
+	}
+
+}
---
+++ src/exercise/ArrayEx13.java
@@ -0,0 +1,14 @@
+package exercise;
+
+public class ArrayEx13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("매개변수의 개수 : " + args.length);
+		for(int i=0; i<args.length; i++){
+			System.out.println("args[" + i + "] = \"" + args[i] + "\"");
+		}
+
+	}
+
+}
