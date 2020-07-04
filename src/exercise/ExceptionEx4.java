0f93c45ce81df828270ed1f0690de81ad962150a
박우재 <@DESKTOP-5O4ET8N>
2020-07-05 02:23:43 +0900

    예외 처리

---
+++ src/exercise/ExceptionEx4.java
@@ -0,0 +1,17 @@
+package exercise;
+
+class ExceptionEx4 {
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+		
+		try{
+			System.out.println(3);
+			System.out.println(0/0);
+			System.out.println(4);
+		} catch(ArithmeticException e){
+			System.out.println(5);
+		}
+		System.out.println(6);
+	}
+}
---
+++ src/exercise/RP_ExceptionEx1.java
@@ -0,0 +1,19 @@
+package exercise;
+
+class RP_ExceptionEx1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			try{} catch(Exception e){}
+		} catch(Exception e){
+			try{} catch(Exception f){}
+			
+		}
+		
+		try{
+			
+		} catch(Exception e){
+			
+		}
+	}
+}
