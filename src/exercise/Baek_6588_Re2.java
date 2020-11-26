336e4fb0cd9455d604077ec180e381b419890092
박우재 <@DESKTOP-5O4ET8N>
2020-11-26 17:18:38 +0900

    골드바흐

--- src/exercise/Baek_6588_Re2.java
+++ src/exercise/Baek_6588_Re2.java
@@ -1,10 +1,34 @@
 package exercise;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Baek_6588_Re2 {
-
+	public static final int MAX = 1000000;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		boolean[] check = new boolean[MAX+1];
+		ArrayList<Integer> prime = new ArrayList<Integer>();
+		check[0] = check[1] = true;
+		
+		for(int i=2; i*i<=MAX; i++){
+			if(check[i]==true) continue;
+			prime.add(i);
+			for(int j=i+i; j<=MAX; j+=i){
+				check[j]=true;
+			}
+		}
+		
+		while(true){
+			int n = sc.nextInt();
+			if(n==0) break;
+			for(int i=1; i<prime.size(); i++){
+				int p = prime.get(i);
+				if(check[n-p] == false){
+					System.out.println(n + " = " + p + " + " + (n-p));
+					break;
+				}
+			}
+		}
 	}
-
 }
